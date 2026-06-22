09c05c0b8606dabd6da05c7f0aefedff5f1a85db
frenchnganu-lab <frenchnganu@gmail.com>
2026-06-22 15:33:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (tg) {
 const form = document.getElementById("leadForm");
 const result = document.getElementById("result");
 
-const MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/cr7vjbingws8zj2jk2xwqc94zmxqjdns";
+const MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/cr7vjbingws8zj2jk2xwqc94zmxgidns";
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
